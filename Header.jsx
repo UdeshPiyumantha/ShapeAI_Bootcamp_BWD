365066b0ac1d54e93d5e52ff102ca7f289633759
Udesh Piyumantha <53423260+UdeshPiyumantha@users.noreply.github.com>
2021-06-05 16:43:29 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Javascript and React.js 7 Days Bootcamp by ShapeAI</h1>
+    </header>
+  );
+}
+
+export default Header;
